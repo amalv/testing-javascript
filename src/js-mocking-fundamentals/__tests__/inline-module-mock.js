038e9baa175434b3473d773c55f49c7f0c4958a5
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2019-01-13 23:22:37 +0100

    Fix eslint issues

---
+++ src/js-mocking-fundamentals/__tests__/inline-module-mock.js
@@ -0,0 +1,21 @@
+const thumbWar = require('../thumb-war');
+const utilsMock = require('../utils');
+
+jest.mock('../utils', () => {
+  return {
+    // eslint-disable-next-line
+    getWinner: jest.fn((p1, p2) => p1),
+  };
+});
+
+test('returns winner', () => {
+  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler');
+  expect(winner).toBe('Kent C. Dodds');
+  expect(utilsMock.getWinner.mock.calls).toEqual([
+    ['Kent C. Dodds', 'Ken Wheeler'],
+    ['Kent C. Dodds', 'Ken Wheeler'],
+  ]);
+
+  // cleanup
+  utilsMock.getWinner.mockReset();
+});
